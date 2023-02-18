2c0dec7e299a46661c040cd6b27eaefccb008a63
Deepak Tiwari <deepurajpandit2@gmail.com>
2023-02-18 22:31:34 +0530

    shortingArrays

---
+++ Arrays/shortigArray.js
@@ -0,0 +1,7 @@
+var array = [40,30,12,25];
+
+for(var i = 0; i<array.length; i++){
+   for(var j=0; j<array.length; j++){
+    console.log(array[j]);
+   }
+}
--- Arrays/sumTwoEle.js
+++ Arrays/sumTwoEle.js
@@ -12,18 +12,21 @@
 //     }  
 // }
 
+// var array = [2, 12, 5, 7, 8, 9];
+
+// var first = 0;
+// var last = array.length;
+
+// for(var i=first;first<array.length; first++){
+//     if(array[first]+array[last] == 14){
+//         console.log(array[first]);
+//         console.log(array[last]);
+//         // break;
+//     }else{
+//         first++;
+//         last--;
+//     }
+// }
+
 var array = [2, 12, 5, 7, 8, 9];
 
-var first = 0;
-var last = array.length;
-
-for(var i=first;first<array.length; first++){
-    if(array[first]+array[last] == 14){
-        console.log(array[first]);
-        console.log(array[last]);
-        // break;
-    }else{
-        first++;
-        last--;
-    }
-}
